cc52993d444ae3cc810e77805d21267efec25c21
CodingbyDaniel <109753547+Dana-Codes@users.noreply.github.com>
2024-10-11 21:04:21 +0100

    created  middleware folder and added error.ts file to handle Errors

--- backend/middleware/error.ts
+++ backend/middleware/error.ts
@@ -1,11 +1,42 @@
-import { NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/ErrorHandler";
 
-module.exports = (err:any, req:Request, res:Response, next:NextFunction) => {
-	err.statusCode = err.statusCode || 500;
-	err.message = err.message || 'internal server error ';
+export const ErrorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "internal server error ";
 
+  //error WRONG mongoDB id
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
 
-	//error mongoDB id
+  // Duplicate key Error
 
-}
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyvalue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong Jwt
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json web token is invalid, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //JWT expire error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token is expired, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
